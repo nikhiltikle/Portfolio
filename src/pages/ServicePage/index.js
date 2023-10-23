73337589f4c40adc47dfc6bc4ee82e9f3a7eb962
nikhiltikle <nikhiltikle31@gmail.com>
2023-10-23 19:58:40 +0530

    developing service section ui

---
+++ src/pages/ServicePage/index.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { FiArrowRight } from "react-icons/fi";
+import { Link, useLocation } from "react-router-dom";
+import {
+  headingAnimation,
+  sectionBodyAnimation,
+} from "../../hooks/useAnimation";
+import { BottomLine } from "../../components";
+import { services } from "../../constants/service.js";
+
+
+const ServicePage = () => {
+  const [ref, inView] = useInView();
+  const [viewDiv, setViewDiv] = useState(false);
+  const animation = useAnimation();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (inView) {
+      setViewDiv(true);
+    } else {
+      setViewDiv(false);
+    }
+  }, [inView, animation]);
+
+ 
+  return (
+    <div className="py-20 parent"
+    // style={{ backgroundColor: "#313131" }}
+    >
+      <motion.div
+        className="mb-12"
+        initial="hidden"
+        animate={viewDiv && "visible"}
+        variants={headingAnimation}
+        style={{padding:"3rem"}}
+      >
+        <h3 className="text-center text-neutral">What I Provide</h3>
+        <h1 className="text-4xl font-semibold text-center">
+          My <span className="text-primary">Services</span>
+        </h1>
+        <BottomLine />
+      </motion.div>
+
+      <motion.div
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+        ref={ref}
+        initial="hidden"
+        animate={viewDiv && "visible"}
+        variants={sectionBodyAnimation}
+      >
+        {services?.map((service) => (
+          <div
+            key={service.id}
+            className={`${service.id % 2 === 0
+              ? "bg-accent shadow-lg"
+              : "bg-[#313131] shadow-md"
+              } rounded-lg p-6 hover:shadow-primary cursor-pointer duration-300`}
+          >
+            <div className="mb-4 text-center">
+              <span className="inline-block text-5xl text-primary">
+                {service.icon}
+              </span>
+            </div>
+            <h2 className="mb-4 text-2xl font-semibold text-center">
+              {service.title}
+            </h2>
+            <p className="text-neutral">{service.description}</p>
+          </div>
+        ))}
+      </motion.div>
+      {location.pathname === "/" && (
+          <div className="mt-4 text-right">
+            <Link
+              to="/project"
+              className="text-2xl hover:text-primary duration-300"
+            >
+              <button className="primary-button">
+                <span>See All</span>
+                <span>
+                  <FiArrowRight />
+                </span>
+              </button>
+            </Link>
+          </div>
+        )}
+    </div>
+    
+  );
+};
+
+export default ServicePage;
