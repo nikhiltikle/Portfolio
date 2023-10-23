6953bb15df7ce1b10bff95f6908bc5b6c8a1621c
nikhiltikle <nikhiltikle31@gmail.com>
2023-10-23 20:01:33 +0530

    download resume button

--- src/components/molecules/Banner/index.js
+++ src/components/molecules/Banner/index.js
@@ -9,6 +9,7 @@ import coding from "../../../assets/coding.json";
 import "../../../pages/shared/Shared.css";
 import { SecondaryBtn } from "../../../components";
 
+
 const Banner = () => {
   const defaultOptions = {
     loop: true,
@@ -17,6 +18,14 @@ const Banner = () => {
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
+  };
+  const pdfURL = 'resume.pdf'; // URL to your PDF
+
+  const downloadPDF = () => {
+    const link = document.createElement('a');
+    link.href = pdfURL;
+    link.download = 'resume_nikhil_tikle.pdf'; // Specify the filename for the downloaded file
+    link.click();
   };
   return (
     <div className="parent min-h-[100vh] flex flex-col-reverse lg:flex-row items-center justify-between">
@@ -28,6 +37,8 @@ const Banner = () => {
         <h2 className="text-neutral text-xl font-medium translate-y-[-90%] sm:translate-y-[-0%]">Hello, I'm</h2>
         <h1 className="text-4xl font-semibold mb-0 translate-y-[-50%] sm:translate-y-[-0%]">Gilbert Hutapea</h1>
         <div className="my-4">
+
+
           <TypeAnimation
             className="text-2xl text-primary font-bold translate-y-[-80%] sm:translate-y-[-0%]"
             cursor={true}
@@ -41,6 +52,7 @@ const Banner = () => {
             ]}
             wrapper="div"
             repeat={Infinity}
+
           />
         </div>
         <p className="text-neutral max-w-xl mb-6 font-medium translate-y-[-20%] sm:translate-y-[-0%]">
@@ -53,18 +65,17 @@ const Banner = () => {
           dedicated to explore New Tools And Technologies. */}
         </p>
 
+
         <div className="flex items-center translate-y-[-60%] sm:translate-y-[-0%]">
-          <a
-            href="https://drive.google.com/file/d/19rnbukAhf9oPhadMhsvI3xnWF6FIYeMT/view?usp=share_link"
-            target="blank"
-          >
-            <button className="primary-button">
-              <span>My Resume</span>
-              <span>
-                <FaDownload />
-              </span>
-            </button>
-          </a>
+
+          <button className="primary-button" onClick={downloadPDF}>
+
+            <span>My Resume</span>
+            <span>
+              <FaDownload />
+            </span>
+          </button>
+
           <Link to="/about" className="ml-4">
             <SecondaryBtn>
               <span>About Me</span>
