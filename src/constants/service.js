c488a242350dc97d24d66a3b53b2402ad744a0b4
nikhiltikle <nikhiltikle31@gmail.com>
2023-10-23 19:57:23 +0530

    added service content

---
+++ src/constants/service.js
@@ -0,0 +1,59 @@
+import { DiAtom } from "react-icons/di";
+import { TbExchange } from "react-icons/tb"
+import { MdDeveloperMode } from "react-icons/md"
+import { VscDebugConsole } from "react-icons/vsc"
+import { CgPerformance } from "react-icons/cg"
+import { BiCustomize } from "react-icons/bi"
+import { MdIntegrationInstructions } from "react-icons/md"
+
+export const services = [
+  {
+    id: 1,
+    title: "Front-end Architecture & Design",
+    icon: <DiAtom />,
+    description:
+      "As a Developer, I offer a distinguished expertise in front-end architecture and design. My commitment to excellence is evident in my work, which showcases a profound focus on user-centric solutions, precision in crafting intuitive interfaces, adeptness in responsive design, and mastery of contemporary web technologies to optimize user experiences. ",
+  },
+  {
+    id: 2,
+    title: "Custom App Development",
+    icon: <BiCustomize />,
+    description:
+      "I collaborate closely with clients to discern their objectives and then design and develop tailored solutions. This encompasses wireframes, and prototypes, utilizing Bootstrap and Material UI, and integrating third-party functionalities such as maps and chats. My focus is on creating visually captivating, intuitive, and user-friendly UI designs.",
+  },
+  {
+    id: 3,
+    title: "Cross-Browser Compatibility",
+    icon: <TbExchange />,
+    description:
+      "In the world of Cross-Browser Compatibility, I specialize in creating seamless digital solutions that work flawlessly across a spectrum of web browsers. My track record proves my knack for eliminating compatibility issues through meticulous testing and adherence to web standards. I'm dedicated to delivering digital experiences that captivate and engage users universally, transcending browser limitations.",
+  },
+  {
+    id: 4,
+    title: "User Interface Development",
+    icon: <MdIntegrationInstructions />,
+    description:
+      "I specialize in crafting UI interfaces that are both highly functional and visually captivating. My secret sauce? I dive deep into understanding your unique needs, transforming them into user-friendly interfaces that go beyond expectations. It's all about you, the user - I design with your experience in mind, ensuring seamless interaction. I'm on a relentless quest for perfection, rigorously refining designs for peak performance, delivering an unmatched user experience",
+  },
+  {
+    id: 5,
+    title: "Mobile Responsiveness",
+    icon: <MdDeveloperMode />,
+    description:
+      "design techniques, flexible grid systems, and media queries to ensure optimal user experiences across devices and browsers. My commitment to excellence is underscored by extensive testing, which guarantees the responsiveness and user-friendliness of every website I design, regardless of the user's choice of device or browser.",
+  },
+  {
+    id: 6,
+    title: "Debugging and Testing",
+    icon: <VscDebugConsole />,
+    description: "As a software developer, I strongly emphasize rigorous testing and debugging to enhance the quality and reliability of software applications. I employ a range of techniques, including unit, integration, and acceptance testing, to efficiently pinpoint and rectify errors and issues in web pages and applications."
+    ,
+  },
+  {
+    id: 6,
+    title: "Performance Optimization",
+    icon: <CgPerformance />,
+    description: "Within the domain of Performance Optimization, I excel in the art of refining digital experiences for peak speed and efficiency. My portfolio exemplifies a history of elevating website and application performance through methodical analysis, strategic enhancements, and judicious resource allocation. My dedication lies in providing seamless, high-velocity digital solutions that captivate and retain users."
+    ,
+  },
+];
