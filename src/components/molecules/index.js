7eaa293c3a69ee90928a194ecff613d5c4bf18b9
nikhiltikle <nikhiltikle31@gmail.com>
2023-10-31 17:39:32 +0530

    cleanup of unwanted files

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -6,7 +6,6 @@ import MernBlogRepair from './MernBlogRepair';
 import MySkill from './MySkill';
 import Navbar from './Navbar';
 import NotFound from './NotFound';
-import ProjectDetails from './ProjectDetails';
 import Service from './Service';
 
 export {
@@ -18,6 +17,5 @@ export {
   MySkill,
   Navbar,
   NotFound,
-  ProjectDetails,
   Service,
 };
