bbd992a9b06887237c6b425e499e06bfe66438ef
nikhiltikle <nikhiltikle31@gmail.com>
2023-10-31 17:43:11 +0530

    updating project section with hide and read more button

--- src/pages/Project/index.js
+++ src/pages/Project/index.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from "react";
-import Items from "../../Utils/Items";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import { Link, useLocation } from "react-router-dom";
-import { FiArrowRight } from "react-icons/fi";
-import "./Project.css";
+import React, { useEffect, useState } from 'react';
+import Items from '../../Utils/Items';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useLocation } from 'react-router-dom';
+import './Project.css';
 import {
   headingAnimation,
   sectionBodyAnimation,
-} from "../../hooks/useAnimation";
-import { BottomLine } from "../../components";
+} from '../../hooks/useAnimation';
+import { BottomLine } from '../../components';
+import ProjectCard from './ProjectCards';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Project = () => {
   const [items, setItems] = useState(Items);
-  const [activeBtn, setActiveBtn] = useState("all");
   const location = useLocation();
   const [ref, inView] = useInView();
   const [viewDiv, setViewDiv] = useState(false);
@@ -25,32 +25,24 @@ const Project = () => {
     } else {
       setViewDiv(false);
     }
-    if (location.pathname === "/" && items.length > 3) {
+    if (location.pathname === '/' && items.length > 3) {
       setItems(items.slice(0, 3));
     }
   }, [inView, animation, location, items]);
 
-  const filterItem = (category) => {
-    const filtered = Items.filter((item) => item.category === category);
-    setItems(filtered);
-    if (filtered.length > 3 && location.pathname === "/") {
-      setItems(filtered.slice(0, 3));
-    }
-  };
-
   return (
-    <div className={`${location.pathname !== "/" && "pt-16"}`}>
+    <div className={`${location.pathname !== '/' && 'pt-16'}`}>
       <div className="parent py-12">
         <motion.div
           initial="hidden"
-          animate={viewDiv && "visible"}
+          animate={viewDiv && 'visible'}
           variants={headingAnimation}
         >
           <div className="mb-12">
             <h3 className="text-neutral text-center">
               Some of my recent Projects
             </h3>
-            <h1 className="text-4xl font-semibold text-center">
+            <h1 className="text-4xl font-semibold text-center px-4">
               Featured <span className="text-primary">Projects</span>
             </h1>
             <BottomLine />
@@ -60,110 +52,28 @@ const Project = () => {
         <motion.div
           ref={ref}
           initial="hidden"
-          animate={viewDiv && "visible"}
+          animate={viewDiv && 'visible'}
           variants={sectionBodyAnimation}
-        >
-          <div className="mt-6 mb-2 flex items-center justify-center flex-wrap">
-            <button
-              className={`btn btn-sm bg-primary border-2 border-primary text-white hover:bg-transparent hover:border-primary duration-300 mx-3 my-3 sm:my-0 ${
-                activeBtn === "all" && "active-btn"
-              }`}
-              onClick={() => {
-                setActiveBtn("all");
-                location.pathname === "/"
-                  ? setItems(Items.slice(0, 3))
-                  : setItems(Items);
-              }}
-            >
-              All
-            </button>
-            <button
-              className={`btn btn-sm bg-primary border-2 border-primary text-white hover:bg-transparent hover:border-primary duration-300 mx-3 my-3 sm:my-0 ${
-                activeBtn === "business" && "active-btn"
-              }`}
-              onClick={() => {
-                setActiveBtn("business");
-                filterItem("business");
-              }}
-            >
-              Business
-            </button>
-            <button
-              className={`btn btn-sm bg-primary border-2 border-primary text-white hover:bg-transparent hover:border-primary duration-300 mx-3 my-3 sm:my-0 ${
-                activeBtn === "personal" && "active-btn"
-              }`}
-              onClick={() => {
-                setActiveBtn("personal");
-                filterItem("personal");
-              }}
-            >
-              Personal
-            </button>
-            <button
-              className={`btn btn-sm bg-primary border-2 border-primary text-white hover:bg-transparent hover:border-primary duration-300 mx-3 my-3 sm:my-0 ${
-                activeBtn === "game" && "active-btn"
-              }`}
-              onClick={() => {
-                setActiveBtn("game");
-                filterItem("game");
-              }}
-            >
-              Game
-            </button>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {items.map((item) => (
-              <motion.div
-                initial={{ x: 200, opacity: 0, scale: 0 }}
-                animate={{
-                  x: 0,
-                  scale: 1,
-                  opacity: 1,
-                  transition: { duration: 0.3 },
-                }}
-                key={item.id}
-                className="item-container rounded-lg shadow-lg p-3 flex flex-col justify-between hover:shadow-primary duration-500"
-                style={{ backgroundColor: "#313131" }}
-              >
-                <div className="item h-full">
-                  <img
-                    className="rounded-lg h-full w-full"
-                    src={item.mainImage}
-                    alt={item.title || "Item Image"}
-                  />
-                  <div className="overlay">
-                    <h3 className="text-2xl text-primary font-semibold">
-                      {item.title}
-                    </h3>
-                    <Link
-                      to={`/project/${item.id}`}
-                      className="mt-3 inline-block"
-                    >
-                      <button className="btn btn-sm border-2 border-transparent bg-primary hover:bg-transparent text-white hover:border-primary duration-500">
-                        See Details
-                      </button>
-                    </Link>
-                  </div>
-                </div>
-              </motion.div>
+        />
+        <Container>
+          <Row className=" flex justify-center	">
+            {Items.map((project) => (
+              <Col md={4} className="project-card">
+                <ProjectCard
+                  imgPath={project.imgurl}
+                  isBlog={false}
+                  title={project.name}
+                  description={project.description}
+                  moreDescription={project.moreDescription}
+                  ghLink={project.ghLink}
+                  demoLink={project.demoLink}
+                  customButton={project.buttonLabel}
+                  icon={project.icon}
+                />
+              </Col>
             ))}
-          </div>
-        </motion.div>
-        {location.pathname === "/" && (
-          <div className="mt-4 text-right">
-            <Link
-              to="/project"
-              className="text-2xl hover:text-primary duration-300"
-            >
-              <button className="primary-button">
-                <span>See All</span>
-                <span>
-                  <FiArrowRight />
-                </span>
-              </button>
-            </Link>
-          </div>
-        )}
+          </Row>
+        </Container>
       </div>
     </div>
   );
