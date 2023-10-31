a405b261ad3b3dddd721bc68986d10a5076d00ab
nikhiltikle <nikhiltikle31@gmail.com>
2023-10-31 17:43:55 +0530

    removing unwanted content

---
+++ src/pages/Project/ProjectCards.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { CgWebsite } from 'react-icons/cg';
+
+function ProjectCards(props) {
+  const [expandedText, setExpandedText] = useState(false);
+
+  return (
+    <>
+      <Card className="project-card-view">
+        <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+        <Card.Body>
+          <Card.Title
+            className="mb-3 text-2xl text-[#ff651c] font-bold"
+            style={{ textAlign: 'center' }}
+          >
+            {props.title}
+          </Card.Title>
+
+          <Card.Text
+            style={{ textAlign: 'left', padding: '0 20px' }}
+            className={expandedText ? 'expanded-card-text' : ''}
+          >
+            {props.description}
+          </Card.Text>
+
+          <button
+            onClick={() => setExpandedText(!expandedText)}
+            className="text-[#ff651c] px-5"
+          >
+            {expandedText ? 'Hide' : 'Read more'}
+          </button>
+
+          <div className="flex justify-center	py-6">
+            {!!props.demoLink && (
+              <Button variant="primary" href={props.demoLink} target="_blank">
+                {props.icon || <CgWebsite />}&nbsp;
+                {props.customButton || 'Website'}
+              </Button>
+            )}
+          </div>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
+export default ProjectCards;
